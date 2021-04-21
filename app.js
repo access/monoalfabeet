ec3bb5252b3dfc810a0b5ba1621c4a298980400a
access <tommi_versetti@list.ru>
2021-04-21 20:25:36 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,168 @@
+const alphaBet = 'ABDEFGHIJKLMNOPRSŠZŽTUVÕÄÖÜ'.split('');
+const qalphaBet = 'ÜÖÄÕVUTŽZŠSRPONMLKJIHGFEDBA'.split('');
+/////const ctext = 'gšnükäžxdk vrlagp pxnqap, xl rššnwüklk tkglwpl, xl pkkmxn xuykn šu ukgklaüwpxy xpwknqa gšulmšnnw knn, šnw xxplw xpwüxux gšmššukdwwmapx šrdxm äžmwl oapl rwwaükknl. „xüšlpwšukknpxnl šu pxx mkpgx rxlg gšqa xxplwnx, pxyk xukü rwwaükknx,“ gwmoalkp dknnkdkuxü ok gmwwpwgšüwpošuw oarl lššükp mfra pšlpwkknüxxywkp lxrlay äzzmyaüwpxp. nwpkgp šu ävroapl šnnk üamxnwg gk pxnnxäžmkpl, xl dkmkpxüky gmwwpwy šu äwqxü užwykuay: lkkplaüwplkpx pkkmx ükokuyapxp šu knklw ükyknkü, gaw xuux gmwwpw ok džrxü gaw 10 000 xnkuwgaqk pkkmxn kuukh gk üvuxgfüux lzzgšrk gkyaüwux dknapknl lauyk. rwwynkpxy pššdwpwy frxuyapx äwwmküwpl pkkmxükkqk lxqxnwganl oahk xuux dknwlpapx šlpapl, pxpl lxkdx pxkn dvmgäknnwdvwplnapxpl knqapx pkkuay ukgklaüwplxpl nxdwp. mwwqw gmwwpwgšüwpošuw gšmmmknyap ükuymwnl pkkmlxnx nwwgaüwpl äwwmklk lvw pwwpgw pxqkyapl fgpokqa. xüükplx špkdknnkdkuxük rxmqš lkpaok pvuan lanw xpwüxpxn äwwmkuqa äžxdkn dknnk äššnl pwppx pxklay wutšlxnxtšuwnx üvux lauuwqk 600 gvuxl, üwnnxp aamwlw äwwmkuqalx gxrlwüwpl ok dvwüknwggx xmkuyxwy. pkkmx nagga äkuxg lanw lxqxnwganl gxmqxuyapxuk, pxpl xmwšnagšmmk gxrlxplküwpxn šnw xhkpxnqx, gkp ok gawykp wuwüxpxy, gxnnxn rwwaükkn padxgšya, pwwk wpšnklpwššuw pkkdky lannk ok üwnnwux šu gwuynap, xl uky xuykqk rkwqapl gkkpk xw lšš. ämkxqa šu nwwgaüwpäwwmkuqa knapxgp knknwux xnagšrl ok gxrlwh ävrwüvlx, xl fnyoaran pkkh ämkküwqk pvwlk frx gšmmk – gkp ükuymwnx dvw pwwp rwwaükknx gšoa. rwwaükk xllxdvloklx nwwya lxqxdoarl kmuš gaapg flnxp, xl wnüpxnqxnl šu gvwg pkkmx xllxdvloky gxxmanwpxp šnagšmmkp ok lxqalpxdky lauy ok äžxd gšmmkqk, lxkyüklk, üwp ršüux xuxpxqk gkkpk lššh. „šnxüx knnxp gmwwpw knqapxp, üvoay kdknyadky ožmg-ožmqanl ok ämkxqa äxkh šnxük lžrxnxäkua gxpgäauglwp wuwüxplx lxmdwp. xllxdvlxlx lxqadap ožlgah dvwüknapl üzzyk,“ nkapap gaapg. xkp-w kmxuyapgxpgaplx dvmqaplwg šu kdkuay gmwwpwkhwgxpgap.xx dxxhwnxrx, üwnnx gkaya äknalw gk rwwaükk xllxdvloklxn šnagšmmk gšrlk lkqkpwpwyxl kuyk. dkplkuay xllxdvloky šnwy rwuukuqalxp xllxdkklnwgay. „gvwg pvnlah pxnnxpl, gawykp ožždky lšwüwük xamššäk pwpxpxy lkmuxkrxnky, üwp šu šnanwpxy uww lššmkwux pkkmxnx pkküwpxgp gaw gk lššykuqa džnokdxšgp,“ nkapap kpw ykqzänkpl lxqxdoarl gandš äxuymk. gžwukp kpadkp xllxdvllxp lzzlkh 90 wuwüxpl. „šnxüx äwwmkuay wuwüxplx ükokpwpxpl nwwgaüwpl xmw špkgšuykyx dkrxn ok pxx pkkh lšwüayk dkwy lzzdkokyaplxpl nžrladknl. lššlüwpxllxdvllxp kqk xw šnx gšyagšulšm paamxük ranqk wuwüxplx äaran nkrxuyapxgp. pxx, üwyk gmwwp lxqxnwganl xllxdvllxnx lžrxuykh, pkkh pxnqxgp pfqwpxgp,“ mžžgwp gžmynkp 190 wuwüxpxnx lzzy kuydk kpw ü ok ä uampl oarklapx nwwqx kqam uamp. rwwaükk frx paamwük lamwpüwxllxdvlok šf ükwuxqmaää šükuwg pkuyxm gšänw šnw gšugmxxlpxü: „lxqxüwpl šu gklkplmšštwqk, üwp xllxdvllxnx oahk lššuay gkroa gfüuxwy larkuyxwy. xuküap lzzlkoklxpl šu pauyäargapxnx pkkyxlay. gaw nžrxük gaa oššgpan dknwlpapxnl üwuqxwy gšugmxxlpxwy nxxdxuyapüxxlüxwy xw lanx, äšnx üaay dkmwkulw, gaw wuwüxpxy lzznl dkhkplkyk,“ lxklkp lk. rwwaükk lkmhwoklx frwplal oarlwd gkok kulšup flnxp, xl gk rwwaükknx ovaywp xmwšnagšmmk gxrlwüwpx ožmxn gxmqx šplaäkkuwgk, üwp ämkxqapxgp mkrauxuay. „gkaänaplxp yxpwutwlpxxmwüx äwuykpwy pkqxyküwuw ok gnwxulwyxqk gšgga äaaladklxnx lzzlkoklxnx šu okqklay užšükpgwy uwuq yxpwutwlpxxmwüwpdkrxuywy, “ mžžgwp lk. pküalw šu uxxy kalšoarlwyxn, gxp lxxdky gkahkdxyapwy ükuymwnl. gššplzzp dknnkqk šu knaplklay khwdkokoklxnx lšwyakwuxlx gšoalšwüxlküwplxxuapxqk. kulšup nwpkp, xl knklxp pxnnxpl užyknkpl šu rwwaükknqw dvwüknwg x-äšx gkaya gvwqwn šplx pššmwlkyk uww, xl wuwüxplxdkrxnwpxy gšulkglwy šu üwuwükknpxy. rwwaükk kmxuyapgxpgapx oarklkok nwwp mxüüxnq gwuuwlkp, xl xllxdvloklxnl gšqalay kuyüxy xykplklkgpx ükokuyap- ok gšüüauwgklpwššuwüwuwplxxmwaüw gkaya dknwlpapxnx. xllxdvloklx gšgga kmdklay gkroayx gšüäxupxxmwüwux pvnlah dknwlpapx šlpaplxpl ükokuyapx lšxlapxgp. gfnn kqk šu rwwaükk xllxdvloklxn ržplw üxxnxp pxx, gawykp gkgp kkplkl lkqkpw ükykn dxxlkpx rwwaükk ok ükuymw dkrxn nwwgnapl lkgwplkp. gk pwwp kmdklw ganay gšgga, gawy nväägšggadvllxp xw gšüäxupxxmwlay üwykqw. lamwpüwršškok špkp šnnkgpx ämkxqa pkkmxn žmkššlkdky, pxpl rkwqapäarkuqa lkkuyaüwpx kxqk xw lxk gxxqw. šf lwwl mxwpwy oarklapx xpwüxxp lwwl mkuyükk uxulwp, xl aapw lxnnwüapw happwtwmüknx ämkxqa gxxqw xw lxx uwuq gvwg lxrlay lxnnwüapxy šu lfrwplklay ok mkrk lanxh lkqkpw ükgplk. „pkkh šnxük džqk gxxmanwux, gaw pxnnwux šnagšmy äwgknl ožlgah,“ nkapap lk. pkükp pxwpap šu gk pkkmx ükoalapxllxdvllxy. lxnnwüapw lfrwplklkgpx ok aapw xw lxrlk. “gaw gmwwp üzzyah ok nwwgaüwpäwwmkuqay nxxdxuxdky, lauyah lvxužšnwux, xl xxplw wuwüxplx džnwpmxwpwyx kmd džrxuxh ok gšmššuk plmxpp dkokh džnok äargküwpl. pxx dvwh paamxuykyk pwpxlamwpüw,” ožžh gžwuk špkdknnkdkuxü šükm oväwpxnq mkranwgagp. lk uwüxlkh frx rwwaükk dvwüknapxuk äxklpxnl dknüwdkl hknlwükkyx gvmqxwük mšuwüwppxwukqk laanxlšmuw. „šnxüx pxnnx šhoxglwqk gk pxnnxp üvllxp vuuxnwgay, xl xuküap äxmpšuknw xw šnx dxxn äknqklay ok paamw äfpwganapwy ämkxqa äxkn xw šnx.“ rwwaükky ok rwwaükk pväma nwwldklxp pšlpwkknüxxywk qmaääwyxp nxdwpwy užyknk knqapxp fnxpgalpxy lkmhwyk gmwwpwkokn äxküwpxnl pkkmx lššloklx lšwyagkaäa. fnywpxnl nššykdky rwwynkpxy kqk, xl dwwmapxožmqpxp ükkwnükp paamxuxh wuwüxplx rang, gxp užxdky paamxpl ükkpl xmknyklay pkkml xnagšrkuk, gap mwpgwy džwgpxüky ok pkkh lxrk gkaqlzzy fnx wulxmuxlw.';
+const ctext = 'hävršo uüwäx hšäxšträohiohäoai zošgvti oägfiuvti šoanv fnäevgi? ošntšoiga hüvhvrigi aivoaigi hšäxšträohiohäoaigi uüwäx štaär zgähznr hš šoanv fnäevgi nšohiga. aiiui fnäevtš hzzoassr ošršri ezzrvri wš ossfvhzdašrifš, hirš hüvhv štaär hnvvo zaoioiga eääräašx. hätš uivi mho eüdvõllnaäov zt evhššwšgvti ešnatingäo, ovvo aiiui itršoa hüvh zgitiõš, ia dlvr mmnvoädaivr dzvrš tvtf nšohig šwšg mdiohzzo dšhhšuš oššrš. ulnhoütšho zt ovvthzdšg avdi wš šõšaär hzzoass. hšo hš xšgav wššuš aänf zt oiggi evvnštfä oiio? niirioa hidavuš dšhštär evvnštfär zaoioiga aänfi iv eääräaš (õš 2+2 niifgvr), hävr õššašuš oiggigi zgiui wäxš hšoäaäoigi õüatär invtiõšvr uiiauivr, ia aänä mmntvhi šxvoašrš tvtf vtvuioai ainõvoi iioa dzzgvaoirš. tvv tlvaiho zgiui hvttv ešttär xšgav wššuš aänä aivoi hznnäoi (õš štavvfvzoš wš nioaznštv azva\'o, uvggig uügiušg zt inšgrv ovooielloär üäioa), egšštvui oägfirš 0 hznnäoi ezir (õš oigõin) wš aiitäoir tvtf vgušri ozzwitirio zt õüvušgvh uivg tlvaiho wäänõvgwšaltšõ hš aänä õlgvzošooi gvvfäašrš. aänä altšõšazvrä õšguvoašwšr aizoašõšr švtäga hššošummhv tvtf ošuäav iiušgršovui aänä ovoišgšriga hüvh gšäšr wš azzgvr, ia vtvuioir iv wllho altšoio zgähznnšo aänägi šifš õiiauš. dzvšui zgähznnšg wzzhoõšga ovguš eišg tvtf mnvašui givrš tvv nšdõš ainõvoi häv hš mmntvhi wšzho õüvušgvhäga uüvoagvhi gšditräov. häv hiinägvti, hšggvo, šwšušdähšo zgv oiggvoa uääaäoa iggä õvvš? iho zgähznr zt hüvfv wšzho ääo tvtf hiifv õšnšoiušga oiggvoivr uääršaäov gvuvaiinvaär šwš wzzhoäg iggä häaoätär iv zgi. hüvfi šwšušdähšu zoš zt hvtrgšoav zuš dišri mmntvhi wš hzzoassešnatinvaifš oädagiuvti. mnvašui oirš aidš õüvušgvhäga einoztššgoiga tvtf uüvoagvhäga. enzšhavvõoiga oädigrio oššui zgähznnš hiinähäoio ovvohv hitšoav dšhhšuš. häv hšäš oiggvti zgähznr hioaš õüvho? ui iv zgi hvtrgšoav üvfir vtvuioir, higgiga hnvvov enzftzzoi hmovrš. iovšgfä gldaäui õšgvaoäoi zaoäoioa, uvo hidavx wlnfuvoir hšho tlršgša. oiiwlnig õššašui iršov. gzzriašõšoav emovõšr vtvuioir hitšoav hzrärio, dzzgvaoiõšr zuš ainõvoi iioa tvtf oigwšašui hnvvov mdiohzzo zzršaäoa hvvniuvtv. uvho zt hiohäoai oäänio zošo oägfiuvti õšgi? aišt, ia hiohäoir mgrvoiga iv ezzgrštär oirš. nšdõš ainõvoi oivoähzdšga zgv hiohäoai oägfiuvti švtäüvfi ošuu. evfiu zt ezgiiuvhša aihvaštär invoäoir, uvo uütirig ezzrvrig wš aiitäoaig gäxšõšr šõšaäho wllrš, hävr aivoaig uvaai. gvošho zt hš ixšoigfi, uvo giiõiträouiiauivr dšhšašhoi iaaiõüaiaigi õšgvaäoi ezzga ešhhäuš. ošušo, häv tvv zt hznr hidaioašaär, ovvo aägix oirš wlnfvrš tvtf czzhäo oišrš hnvvov hvvnigi oigwšašuvoigi.';
+//const ctext = 'yxpwutwlpxxmwüwpdkrxuywy';
+const newBet = new Map();
+// for (let i = 0; i < alphaBet.length; i++) {
+//   const char = alphaBet[i];
+//   newBet.set(char.toLowerCase(),qalphaBet[i].toLowerCase());
+
+// }
+// let newText = '';
+// ctext.split('').forEach(char => {
+//   const newChar = newBet.get(char.toLowerCase());
+//   if (newChar != undefined)
+//     newText += newChar
+//   else
+//     newText += char;
+// });
+// console.log(newText);
+
+newBet.set('a', 'a');
+newBet.set('b', 'b');
+newBet.set('d', 'd');
+newBet.set('e', 'o');
+newBet.set('f', 'f');
+newBet.set('g', 'g');
+newBet.set('h', 'h');
+newBet.set('i', 'i');
+newBet.set('j', 'j');
+newBet.set('k', 'k');
+newBet.set('l', 'l');
+newBet.set('m', 'ü');
+newBet.set('n', 'õ');
+newBet.set('o', 'e');
+newBet.set('p', 'p');
+newBet.set('r', 'r');
+newBet.set('s', 's');
+newBet.set('š', 't');
+newBet.set('z', 'z');
+newBet.set('ž', 'ž');
+newBet.set('t', 'š');
+newBet.set('u', 'u');
+newBet.set('v', 'v');
+newBet.set('õ', 'n');
+newBet.set('ä', 'ä');
+newBet.set('ö', 'ö');
+newBet.set('ü', 'm');
+
+// newBet.set('õš', 'nt');
+// newBet.set('õš', 'vm');
+// newBet.set('õš', 'vt');
+//console.log(alphaBet.reverse().join(''));
+
+
+let newText = '';
+ctext.split('').forEach(char => {
+  const newChar = newBet.get(char.toLowerCase());
+  if (newChar != undefined)
+    newText += newChar
+  else
+    newText += char;
+});
+console.log(newText);
+
+
+/*
+
+
+let wordMaxLen = 0;
+let wordMax = '';
+let wordPreMax = ''; // yxpwutwlpxxmwüwpdkrxuywy
+
+
+ctext.split(' ').forEach(word => {
+  if (word.length > wordMaxLen) {
+    wordMaxLen = word.length;
+    wordPreMax = wordMax;
+    wordMax = word;
+  }
+});
+const cipherBet = new Set();
+wordMax.split('').forEach(c => cipherBet.add(c));
+
+
+const bruteBetSet = [];
+
+// console.log(wordMax);
+// console.log(wordPreMax);
+
+
+
+
+
+
+
+
+
+
+
+
+const onDocReady = function () {
+  const rootEl = document.getElementById("root");
+  let outHtml = '';
+  //rootEl.innerHTML = ctext + '<br>';
+  for (let i = 0; i < cipherBet.length; i++) {
+    const cipherChar = cipherBet[i];
+
+
+    for (let j = 0; j < alphaBet.length; j++) {
+      const alphaChar = alphaBet[j];
+
+      let outText = '';
+
+    }
+  }
+
+  //   alphaBet.forEach(alpha=>{
+  //     //console.log(alpha);
+  //     cipherBet.forEach(cipherChar=>{
+  //       let decText = '';
+  //       ctext.split('').forEach(char=>{
+  //         if(char.toLowerCase===cipherChar.toLowerCase())
+  //         decText+=char;
+  //       });
+  //       outHtml+='';
+  //     });
+  //   })
+
+  // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+    onDocReady();
+  } else {
+    document.addEventListener("DOMContentLoaded", onDocReady);
+  }
+}
+
+*/
---
+++ git_init.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "initial commit"
+git branch -M main
+git remote add origin "https://github.com/access/monoalfabeet"
+git remote -v
+git push -f -u origin main
